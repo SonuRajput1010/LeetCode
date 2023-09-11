2e76b035da2b12821a539ea99a48beb62b8e767e
Sonu Rajput <sonusinghrajput9189@gmail.com>
2023-09-12 04:19:36 +0530

    Added solution - LeetHub

--- Row with max 1s - GFG/row-with-max-1s.cpp
+++ Row with max 1s - GFG/row-with-max-1s.cpp
@@ -6,42 +6,40 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:
-    int lowerBound(vector<int> arr, int n, int x) {
-    int low = 0, high = n - 1;
-    int ans = n;
-
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        // maybe an answer
-        if (arr[mid] >= x) {
-            ans = mid;
-            //look for smaller index on the left
-            high = mid - 1;
+    int lowerBound(vector<int> arr, int n, int x){
+        int low =0;
+        int high = n-1;
+        int ans = n;
+        
+        while(low<=high){
+            int mid = (low+high)/2;
+            
+            if(arr[mid] >= x){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
         }
-        else {
-            low = mid + 1; // look on the right
-        }
+        return ans;
     }
-    return ans;
-    }
-  
+    
 	int rowWithMax1s(vector<vector<int> > arr, int n, int m) {
 	    // code here
-	int cnt_max = 0;
-    int index = -1;
-
-    //traverse the rows:
-    for (int i = 0; i < n; i++) {
-        // get the number of 1's:
-        int cnt_ones = m - lowerBound(arr[i], m, 1);
-        if (cnt_ones > cnt_max) {
-            cnt_max = cnt_ones;
-            index = i;
-        }
-    }
-    return index;
+	    int cnt_max =0;
+	    int index = -1;
+	    
+	    for(int i=0; i<n; i++){
+	        int cnt_ones = m - lowerBound(arr[i],m,1);
+	        if(cnt_ones > cnt_max){
+	            cnt_max = cnt_ones;
+	            index =i;
+	        }
+	    }
+	    return index;
+	    
 	}
-	
 
 };
 
