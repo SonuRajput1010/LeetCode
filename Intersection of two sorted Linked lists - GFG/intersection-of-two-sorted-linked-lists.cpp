ca1dfb2edba88e5ada2320799364c92351a7fbe4
Sonu Rajput <sonusinghrajput9189@gmail.com>
2023-09-16 17:44:40 +0530

    Added solution - LeetHub

--- Intersection of two sorted Linked lists - GFG/intersection-of-two-sorted-linked-lists.cpp
+++ Intersection of two sorted Linked lists - GFG/intersection-of-two-sorted-linked-lists.cpp
@@ -81,20 +81,22 @@ struct Node
 };
 
 */
+
 Node* findIntersection(Node* head1, Node* head2)
 {
     // Your Code Here
     Node* dummy = new Node(-1);
     Node* temp = dummy;
     
-    while(head1 != NULL && head2 != NULL){
+    while(head1 !=NULL && head2 != NULL ){
         if(head1->data == head2->data){
             Node* same = new Node(head1->data);
             temp->next = same;
             temp = same;
             head1 = head1->next;
             head2 = head2->next;
-       }
+        }
+        
         else if(head1->data < head2->data){
             head1 = head1->next;
         }
@@ -104,3 +106,4 @@ Node* findIntersection(Node* head1, Node* head2)
     }
     return dummy->next;
 }
+ 
