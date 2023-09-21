8b97764843d0577cbf4705efbbf7469a6fa5c659
Sonu Rajput <sonusinghrajput9189@gmail.com>
2023-09-21 19:38:54 +0530

    Added solution - LeetHub

--- Merge Without Extra Space - GFG/merge-without-extra-space.cpp
+++ Merge Without Extra Space - GFG/merge-without-extra-space.cpp
@@ -6,26 +6,67 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-        //Function to merge the arrays.
-        void merge(long long arr1[], long long arr2[], int n, int m) 
-        { 
-            // code here 
-            int left = n-1;
-            int right = 0;
-            
-            while(left>=0 && right<m){
-                if(arr1[left] > arr2[right] ){
-                    swap(arr1[left--] , arr2[right++]);
-                }
-                else{
-                    break;
-                }
+    
+          // Optimal solution2
+          
+        void swapIfGreater(long long arr1[], long long arr2[], int ind1, int ind2) {
+            if (arr1[ind1] > arr2[ind2]) {
+                swap(arr1[ind1], arr2[ind2]);
             }
+        }
+
+        void merge(long long arr1[], long long arr2[], int n, int m) {
             
-            sort(arr1,arr1+n);
-            sort(arr2,arr2+m);
-        } 
+            int len = n + m;
+            int gap = (len / 2) + (len % 2);
+        
+            while (gap > 0) {
+                // Place 2 pointers:
+                int left = 0;
+                int right = left + gap;
+                while (right < len) {
+                    if (left < n && right >= n) {
+                        swapIfGreater(arr1, arr2, left, right - n);
+                    }
+                    // case 2: both pointers in arr2[]:
+                    else if (left >= n) {
+                        swapIfGreater(arr2, arr2, left - n, right - n);
+                    }
+                    // case 3: both pointers in arr1[]:
+                    else {
+                        swapIfGreater(arr1, arr1, left, right);
+                    }
+                    left++, right++;
+                }
+                // break if iteration gap=1 is completed:
+                if (gap == 1) break;
+        
+                // Otherwise, calculate new gap:
+                gap = (gap / 2) + (gap % 2);
+            }
+        }
 };
+  
+        // Optimal solution1
+        
+        //  void merge(long long arr1[], long long arr2[], int n, int m) 
+        // { 
+        //     // code here 
+        //     int left = n-1;
+        //     int right = 0;
+            
+        //     while(left>=0 && right<m){
+        //         if(arr1[left] > arr2[right] ){
+        //             swap(arr1[left--] , arr2[right++]);
+        //         }
+        //         else{
+        //             break;
+        //         }
+        //     }
+            
+        //     sort(arr1,arr1+n);
+        //     sort(arr2,arr2+m);
+        // } 
 
 //{ Driver Code Starts.
 
