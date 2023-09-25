c96e5dd23c33ea7fb80b6f1da3f88852c573e491
Sonu Rajput <sonusinghrajput9189@gmail.com>
2023-09-25 07:23:31 +0530

    Added solution - LeetHub

--- Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.cpp
+++ Median of 2 Sorted Arrays of Different Sizes - GFG/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -11,51 +11,52 @@ double MedianOfArrays(vector<int>& array1, vector<int>& array2);
 
 class Solution{
     public:
-    double MedianOfArrays(vector<int>& array1, vector<int>& array2)
+    double MedianOfArrays(vector<int>& a, vector<int>& b)
     {
         // Your code goes here
-        int n1 = array1.size();
-        int n2 = array2.size();
-        
-        int n =  n1 + n2;
-        
-        if(n1 > n2){
-            MedianOfArrays(array2,array1);
-        }
-        
-        int low =0;
-        int high = n1-1;
-        int left = (n1+n2+1)/2;
-        
-        while (low <= high) {
+        int n1 = a.size();
+        int n2 = b.size();
+        if (n1 > n2){
+            return MedianOfArrays(b, a);
             
-            int mid1 = (low + high) / 2;
+        }
+    
+        int n = n1 + n2; //total length
+        int left = (n1 + n2 + 1) / 2; //length of left half
+        
+        //binary search:
+        int low = 0, high = n1;
+        while (low <= high) {
+            int mid1 = (low + high) >> 1;
             int mid2 = left - mid1;
             
             int l1 = INT_MIN, l2 = INT_MIN;
             int r1 = INT_MAX, r2 = INT_MAX;
             
-            if (mid1 < n1) r1 = array1[mid1];
-            if (mid2 < n2) r2 = array2[mid2];
+            if (mid1 < n1) r1 = a[mid1];
+            if (mid2 < n2) r2 = b[mid2];
             
-            if (mid1 - 1 >= 0) l1 = array1[mid1 - 1];
-            if (mid2 - 1 >= 0) l2 = array2[mid2 - 1];
+            if (mid1 - 1 >= 0) l1 = a[mid1 - 1];
+            if (mid2 - 1 >= 0) l2 = b[mid2 - 1];
     
             if (l1 <= r2 && l2 <= r1) {
-                if (n % 2 == 1) return max(l1, l2);
-                else return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
-            }
-                else if(l1>r2){
-                    high = mid1-1;
+                if (n % 2 == 1){ 
+                    return max(l1, l2);
                 }
                 else{
-                    low = mid1+1;
+                    return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+                    
                 }
             }
-        return 0;
+    
+            //eliminate the halves:
+            else if (l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
+        }
+        return 0; //dummy statement
     }
-    MedianOfAr
 };
+
 
 //{ Driver Code Starts.
 
