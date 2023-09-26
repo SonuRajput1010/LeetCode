7e458e0ef66952483611e4c74d9e37abae0c71c5
Sonu Rajput <sonusinghrajput9189@gmail.com>
2023-09-26 21:57:21 +0530

    Added solution - LeetHub

---
+++ Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
@@ -0,0 +1,85 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution{
+    public:
+    // arr[] : int input array of integers
+    // k : the quadruple sum required
+    vector<vector<int> > fourSum(vector<int> &arr, int target) {
+        // Your code goes here
+        
+        int n = arr.size();
+        
+        vector<vector<int>>ans;
+        sort(arr.begin(),arr.end());
+        
+        for(int i=0; i<n; i++){
+            if( i>0 && arr[i] == arr[i-1]) continue;
+            
+            for(int j=i+1; j<n; j++){
+                if(j != (i+1) && arr[j] == arr[j-1]) continue;
+                
+                int k = j+1;
+                int l = n-1;
+                
+                while(k<l){
+                    long long sum = arr[i];
+                sum += arr[j];
+                sum += arr[k];
+                sum += arr[l];
+                
+                if(sum == target){
+                    vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
+                    ans.push_back(temp);
+                    k++;
+                    l--;
+                    
+                    while(k<l && arr[k] == arr[k-1]) k++;
+                    while(k<l && arr[l] == arr[l+1]) l--;
+                }
+                else if(sum < target){
+                    k++;
+                }
+                else{
+                    l--;
+                }
+                }
+                
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k, i;
+        cin >> n >> k;
+        vector<int> a(n);
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        vector<vector<int> > ans = ob.fourSum(a, k);
+        for (auto &v : ans) {
+            for (int &u : v) {
+                cout << u << " ";
+            }
+            cout << "$";
+        }
+        if (ans.empty()) {
+            cout << -1;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
