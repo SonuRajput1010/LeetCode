03c329a3398ada0a52806b45d2d0d96cbca95cba
Sonu Rajput <sonusinghrajput9189@gmail.com>
2023-10-15 06:31:13 +0530

    Added solution - LeetHub

---
+++ Binary Tree to BST - GFG/binary-tree-to-bst.cpp
@@ -0,0 +1,168 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+}; 
+
+void printInorder (struct Node* node)
+{
+    if (node == NULL)
+    return;
+
+    printInorder (node->left);
+    printf("%d ", node->data);
+    printInorder (node->right);
+}
+
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+    return NULL;
+
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current Node
+            currNode->left = new Node(stoi(currVal));
+
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+// } Driver Code Ends
+//User function template for C++
+
+/* The Node structure is
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+}; 
+*/
+
+class Solution{
+  public:
+    // The given root is the root of the Binary Tree
+    // Return the root of the generated BST
+    void inorder(Node* root, vector<int>&ans){
+        if(root == NULL){
+            return ;
+        }
+        inorder(root->left,ans);
+        ans.push_back(root->data);
+        inorder(root->right,ans);
+    }
+    
+    // Once obtained, the root node will always be the middle element in the BST.
+    // Apply this logic to construct the left and right subtrees as well.
+    Node* construct_Tree(vector<int>ans,int l,int r){
+        
+        if(l>r){
+            return NULL;
+        }
+        int mid = l+(r-l)/2;
+        int data = ans[mid];
+        Node* root = new Node(data);
+        root->left = construct_Tree(ans,l,mid-1);
+        root->right = construct_Tree(ans,mid+1,r);
+        
+        return root;
+    }
+    
+    Node *binaryTreeToBST (Node *root)
+    {
+        //Your code goes here
+        vector<int> res;
+        inorder(root,res);
+        sort(res.begin(),res.end());
+        int n = res.size();
+        return construct_Tree(res,0,n-1);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    scanf("%d\n", &t);
+    while (t--)
+    {
+        string s;
+        getline(cin,s);
+        Node* root = buildTree(s);
+        Solution obj;
+        Node *res = obj.binaryTreeToBST (root);
+        printInorder(res);
+        cout<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
