3b910a727f964ff2340e15ca15a032a5cbeba6af
Sonu Rajput <sonusinghrajput9189@gmail.com>
2023-10-30 18:58:20 +0530

    Added solution - LeetHub

---
+++ Convert Min Heap to Max Heap - GFG/convert-min-heap-to-max-heap.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+    void maxHeapify(vector<int>&arr,int i, int n){
+        int largest = i;
+        int left = 2*i+1;
+        int right = 2*i+2;
+        
+        if(left < n && arr[left] > arr[largest]){
+            largest = left;
+        }
+        if(right < n && arr[right] > arr[largest]){
+            largest = right;
+        }
+        if(largest != i){
+            swap(arr[largest],arr[i]);
+            maxHeapify(arr,largest,n);
+        }
+    }
+    void convertMinToMaxHeap(vector<int> &arr, int N){
+        for(int i=N/2; i>=0; i--){
+            maxHeapify(arr,i,N);
+        }
+    }
+    
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while(t--){
+        //Input
+
+       int n; cin >> n;
+       vector<int> vec(n);
+       for(int i = 0;i<n;i++) cin >> vec[i];
+
+        Solution obj;
+        obj.convertMinToMaxHeap(vec,n);
+        for(int i = 0;i<n;i++) cout << vec[i] << " ";
+        cout << endl;
+        
+
+        
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
