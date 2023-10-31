f43004670a25c0f4b01e6668bff922c12faeca3a
Sonu Rajput <sonusinghrajput9189@gmail.com>
2023-10-31 07:51:52 +0530

    Added solution - LeetHub

--- Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
+++ Find All Four Sum Numbers - GFG/find-all-four-sum-numbers.cpp
@@ -12,46 +12,39 @@ class Solution{
     // k : the quadruple sum required
     vector<vector<int> > fourSum(vector<int> &arr, int target) {
         // Your code goes here
-        
-        int n = arr.size();
-        
-        vector<vector<int>>ans;
         sort(arr.begin(),arr.end());
+        int n = arr.size();
+        vector<vector<int>>ans;
         
         for(int i=0; i<n; i++){
-            if( i>0 && arr[i] == arr[i-1]) continue;
-            
+            if(i>0 && arr[i] == arr[i-1]) continue;
             for(int j=i+1; j<n; j++){
-                if(j != (i+1) && arr[j] == arr[j-1]) continue;
-                
+                if(j>i+1 && arr[j] == arr[j-1]) continue;
                 int k = j+1;
                 int l = n-1;
                 
                 while(k<l){
                     long long sum = arr[i];
-                sum += arr[j];
-                sum += arr[k];
-                sum += arr[l];
-                
-                if(sum == target){
-                    vector<int> temp = {arr[i], arr[j], arr[k], arr[l]};
-                    ans.push_back(temp);
-                    k++;
-                    l--;
-                    
-                    while(k<l && arr[k] == arr[k-1]) k++;
-                    while(k<l && arr[l] == arr[l+1]) l--;
+                    sum += arr[j];
+                    sum += arr[k];
+                    sum += arr[l];
+                    if(sum == target){
+                        vector<int> temp = { arr[i], arr[j], arr[k], arr[l] };
+                        ans.push_back(temp);
+                        k++, l--;
+                        while( k<l && arr[k] == arr[k-1]) k++;
+                        while(k<l && arr[l] == arr[l+1]) l--;
+                    }
+                    else if(sum < target){
+                        k++;
+                    }
+                    else{
+                        l--;
+                    }
                 }
-                else if(sum < target){
-                    k++;
-                }
-                else{
-                    l--;
-                }
-                }
-                
             }
         }
+        
         return ans;
     }
 };
