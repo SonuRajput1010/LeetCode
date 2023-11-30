4319332bc49209813804e89aaf66879a3335cf6b
Sonu Rajput <sonusinghrajput9189@gmail.com>
2023-11-30 07:09:22 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -4,42 +4,84 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-  private:
-  bool dfsCheck(int node,int vis[],int pathVis[],vector<int> adj[])
-    {
-        vis[node]=1;
-        pathVis[node]=1;
-        
-        for(auto i : adj[node])
-        {
-            if(!vis[i])
-            {
-                if(dfsCheck(i,vis,pathVis,adj)) return true;
-            }
-            else if(pathVis[i])
-                return true;
-        }
-        
-        pathVis[node]=0;
-        return false;
-    }
   public:
     // Function to detect cycle in a directed graph.
     bool isCyclic(int V, vector<int> adj[]) {
         // code here
-        int vis[V] = {0};
-        int pathVis[V] = {0};
-        
-        for(int i=0; i<V; i++){
-            if(!vis[i]){
-                if(dfsCheck(i,vis,pathVis,adj) == true){
-                    return true;
-                }
+         int inDegree[V] ={0};
+	    for(int i=0; i<V; i++){
+	        for(auto it : adj[i]){
+	            inDegree[it]++;
+	        }
+	    }
+	    
+	    queue<int>q;
+	    int cnt = 0;
+	    
+	    for(int i=0; i<V; i++){
+	        if(inDegree[i] == 0){
+	            q.push(i);
+	        }
+	    }
+	    
+	     while (!q.empty())
+        {
+            int top = q.front(); // front element of queue
+            cnt++;
+            q.pop();
+            // node is in your topo sort so remove it from the indegree
+            for (auto it : adj[top])
+            {
+                inDegree[it]--;
+                if (inDegree[it] == 0)
+                    q.push(it);
             }
         }
-        return false;
+        
+        if(cnt == V){
+            return false;
+        }
+        
+        return true;
+        
     }
 };
+// private:
+//   bool dfsCheck(int node,int vis[],int pathVis[],vector<int> adj[])
+//     {
+//         vis[node]=1;
+//         pathVis[node]=1;
+        
+//         for(auto i : adj[node])
+//         {
+//             if(!vis[i])
+//             {
+//                 if(dfsCheck(i,vis,pathVis,adj)) return true;
+//             }
+//             else if(pathVis[i])
+//                 return true;
+//         }
+        
+//         pathVis[node]=0;
+//         return false;
+//     }
+//   public:
+//     // Function to detect cycle in a directed graph.
+//     bool isCyclic(int V, vector<int> adj[]) {
+//         // code here
+//         int vis[V] = {0};
+//         int pathVis[V] = {0};
+        
+//         for(int i=0; i<V; i++){
+//             if(!vis[i]){
+//                 if(dfsCheck(i,vis,pathVis,adj) == true){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
 
 //{ Driver Code Starts.
 
