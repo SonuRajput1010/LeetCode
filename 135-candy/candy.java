fbc8890fdb13184f629539c9f1f2f6407de62100
Sonu Rajput <sonusinghrajput9189@gmail.com>
2025-12-19 21:23:15 +0530

    Time: 4 ms (25.26%) | Memory: 47.7 MB (34.31%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,14 +1,11 @@
 class Solution {
     public int candy(int[] ratings) {
+
         int n = ratings.length;
         int[] left = new int[n];
-        int[] right = new int[n];
-
-        // int[] ans = new int[n];
 
         left[0] = 1;
         for(int i=1; i<n; i++){
-
             if( ratings[i] > ratings[i-1]){
                 left[i] = left[i-1]+1;
             }
@@ -17,22 +14,23 @@ class Solution {
             }
         }
 
-        right[n-1] = 1;
+        int sum = 0;
+        int curr = 1;
+        int right = 1;
+
         for(int i = n-2; i>=0; i--){
             if( ratings[i] > ratings[i+1]){
-                right[i] = right[i+1]+1;
+                curr = right +1;
+                right = curr;
             }
             else{
-                right[i] = 1;
+                curr = 1;
+                right = 1;
             }
+            sum += Math.max(left[i],curr);
         }
+            sum = sum + Math.max(left[n-1],1);
 
-        int sum = 0;
-
-        for(int i=0; i<n; i++){
-            sum += Math.max(left[i],right[i]);
-        }
-        
         return sum;
     }
 }
