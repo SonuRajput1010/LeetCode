3b2af6ce67aa9d2cea4715fe7efd9d156d7865c0
Sonu Rajput <sonusinghrajput9189@gmail.com>
2025-12-24 10:43:45 +0530

    Time: 1 ms (99.82%) | Memory: 46.3 MB (70.61%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
+
+        for(char ch : magazine.toCharArray() ){
+            freq[ch-'a']++;
+        }
+
+        for(char ch : ransomNote.toCharArray()){
+            if(freq[ch-'a'] == 0){
+                return false;
+            }
+            freq[ch-'a']--;
+        }
+        return true;
+    }
+}
