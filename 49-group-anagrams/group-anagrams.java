e5c247788501c0a71ddc6cda03a41e4090d01515
Sonu Rajput <sonusinghrajput9189@gmail.com>
2026-01-08 22:17:22 +0530

    Time: 7 ms (75.37%) | Memory: 49.8 MB (51.70%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap< String, List<String> > map = new HashMap<>();
+
+        for(String s : strs){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+
+            if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
+
+        
+    }
+}
