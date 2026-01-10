45eacc4530abf2ceeebc5df1698e1cd45deeb3eb
Sonu Rajput <sonusinghrajput9189@gmail.com>
2026-01-10 23:26:13 +0530

    Time: 1 ms (99.80%) | Memory: 46.9 MB (50.62%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -3,18 +3,14 @@ class Solution {
         List<String> ans = new ArrayList<>();
 
         for(int i=1; i<=n; i++){
-            if((i%15 == 0)){
-                ans.add("FizzBuzz");
-            }
-            else if(i%3==0){
-                ans.add("Fizz");
-            }
-            else if(i%5 == 0){
-                ans.add("Buzz");
-            }
-            else{
-                ans.add(String.valueOf(i));
-            }
+
+            if(i%15 == 0) ans.add("FizzBuzz");
+
+            else if(i%3 == 0) ans.add("Fizz");
+
+            else if(i%5 == 0) ans.add("Buzz");
+
+            else ans.add(String.valueOf(i));
         }
         return ans;
     }
