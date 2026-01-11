318973b42c3399356c9f8f15475c2233c737c338
Sonu Rajput <sonusinghrajput9189@gmail.com>
2026-01-12 00:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (37.60%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,17 +1,15 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-         int i = s.length() - 1;
+        int i = s.length()-1;
         int len = 0;
-
-        while (i >= 0 && s.charAt(i) == ' ') {
+        while(i>=0 && s.charAt(i) == ' '){
             i--;
         }
-
-        while (i >= 0 && s.charAt(i) != ' ') {
+        while(i>=0 && s.charAt(i) != ' '){
             len++;
             i--;
         }
-
         return len;
-    }
+
+    }   
 }
