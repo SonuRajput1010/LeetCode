1780605fc6081333abd4b0adace49b642deb9595
Sonu Rajput <sonusinghrajput9189@gmail.com>
2026-07-23 08:45:29 +0530

    Time: 7 ms (67.72%) | Memory: 46.8 MB (40.76%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.java
+++ 1242-matrix-block-sum/matrix-block-sum.java
@@ -1,26 +1,45 @@
 class Solution {
     public int[][] matrixBlockSum(int[][] mat, int k) {
 
-        int m = mat.length;
-        int n = mat[0].length;
+        int n = mat.length;
+        int m = mat[0].length;
 
-        int[][] ans = new int[m][n];
+        int[][] prefixSum = new int[n + 1][m + 1];
 
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-
-                int sum =0;
-                for(int r=i-k; r <= i+k; r++){
-                    for(int c=j-k; c <= j+k; c++){
-                        if(r >= 0 && r < m && c>=0 && c<n){
-                            sum += mat[r][c];
-                        }
-                    }
-                }
-                ans[i][j] = sum;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                prefixSum[i][j] =
+                        mat[i - 1][j - 1]
+                        + prefixSum[i - 1][j]
+                        + prefixSum[i][j - 1]
+                        - prefixSum[i - 1][j - 1];
             }
         }
+
+        int[][] ans = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                int r1 = Math.max(0, i - k);
+                int c1 = Math.max(0, j - k);
+
+                int r2 = Math.min(n - 1, i + k);
+                int c2 = Math.min(m - 1, j + k);
+
+                r1++;
+                c1++;
+                r2++;
+                c2++;
+
+                ans[i][j] =
+                        prefixSum[r2][c2]
+                        - prefixSum[r1 - 1][c2]
+                        - prefixSum[r2][c1 - 1]
+                        + prefixSum[r1 - 1][c1 - 1];
+            }
+        }
+
         return ans;
-        
     }
 }
