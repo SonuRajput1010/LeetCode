74b78b3bc81d654d724c79c45992e5441cb1773a
Sonu Rajput <sonusinghrajput9189@gmail.com>
2026-08-06 07:18:25 +0530

    Time: 34 ms (28.05%) | Memory: 47.3 MB (25.37%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,41 +1,90 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+
+        // Best Approach
         List<Integer> res = new ArrayList<>();
 
-        if(p.length() > s.length() ) return res;
+        if (p.length() > s.length()) return res;
 
-        Map<Character,Integer> pMap = new HashMap<>();
-        Map<Character,Integer> sMap = new HashMap<>();
+        Map<Character, Integer> pMap = new HashMap<>();
 
-        for(char c : p.toCharArray()){
-            pMap.put(c, pMap.getOrDefault(c,0)+1);
+        for (char c : p.toCharArray()) {
+            pMap.put(c, pMap.getOrDefault(c, 0) + 1);
         }
 
-        int l =0; 
-        int count = p.length();
+        int l = 0;
+        int count = p.length();   // FIX 1
 
-        for(int r=0; r<s.length(); r++){
+        for (int r = 0; r < s.length(); r++) {
+
             char ch = s.charAt(r);
 
-            sMap.put(ch,sMap.getOrDefault(ch,0)+1);
+            int val = pMap.getOrDefault(ch, 0);
 
-            if(pMap.containsKey(ch) && sMap.get(ch) <= pMap.get(ch)){
-                count--;
-            }
+            if (val > 0) count--;
 
-            if(r-l+1 > p.length()){
+            pMap.put(ch, val - 1);
+
+            if (r - l + 1 > p.length()) {
+
                 char leftCh = s.charAt(l);
-                if(pMap.containsKey(leftCh) && sMap.get(leftCh) <= pMap.get(leftCh)){
-                    count++;
-                }
-                    sMap.put(leftCh,sMap.get(leftCh)-1);
-                    l++;
-            }
-            if(count == 0) res.add(l);
 
+                int leftVal = pMap.getOrDefault(leftCh, 0);
+
+                if (leftVal >= 0) count++;
+
+                pMap.put(leftCh, leftVal + 1);
+
+                l++;
+            }
+
+            // FIX 2
+            if (r - l + 1 == p.length() && count == 0) {
+                res.add(l);
+            }
         }
 
-        return res;
+    return res;
+    }
+
+    // Better Approach
+
+        // List<Integer> res = new ArrayList<>();
+
+        // if(p.length() > s.length() ) return res;
+
+        // Map<Character,Integer> pMap = new HashMap<>();
+        // Map<Character,Integer> sMap = new HashMap<>();
+
+        // for(char c : p.toCharArray()){
+        //     pMap.put(c, pMap.getOrDefault(c,0)+1);
+        // }
+
+        // int l =0; 
+        // int count = p.length();
+
+        // for(int r=0; r<s.length(); r++){
+        //     char ch = s.charAt(r);
+
+        //     sMap.put(ch,sMap.getOrDefault(ch,0)+1);
+
+        //     if(pMap.containsKey(ch) && sMap.get(ch) <= pMap.get(ch)){
+        //         count--;
+        //     }
+
+        //     if(r-l+1 > p.length()){
+        //         char leftCh = s.charAt(l);
+        //         if(pMap.containsKey(leftCh) && sMap.get(leftCh) <= pMap.get(leftCh)){
+        //             count++;
+        //         }
+        //             sMap.put(leftCh,sMap.get(leftCh)-1);
+        //             l++;
+        //     }
+        //     if(count == 0) res.add(l);
+
+        // }
+
+        // return res;
 
 // Brute Force Approach
 
@@ -58,5 +107,4 @@ class Solution {
         //     if(Arrays.equals(sCount,pCount)) res.add(i);
         // }
         // return res;
-    }
 }
