f3c380d16b34b8dc35d5b07e5f975777d5298881
Sonu Rajput <sonusinghrajput9189@gmail.com>
2026-08-07 03:27:55 +0530

    Time: 74 ms (8.74%) | Memory: 47.9 MB (8.84%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,30 +3,31 @@ class Solution {
 
         int maxLen = 0;
         int l = 0;
+        Map<Character,Integer> map = new HashMap<>();
+        for(int r=0; r<s.length(); r++){
+            char ch = s.charAt(r);
+            map.put(ch,map.getOrDefault(ch,0)+1);
 
-        // for(int r=0; r<s.length(); r++){
+            while(map.get(ch) > 1){
+                char cLeft = s.charAt(l);
+                map.put(cLeft,map.get(cLeft)-1);
+                l++;
+            }
+            maxLen = Math.max(maxLen,r-l+1);
+            
+        }
+
+        // for(int i=0; i<s.length(); i++){
         //     Set<Character> set = new HashSet<>();
-        //     char ch = s.charAt(r);
-        //     if(set.contains(ch)){
-        //         max = Math.max(max,r-l+1);
-        //         l++;
-        //     }
-        //     else{
+        //     for(int j=i; j<s.length(); j++){
+        //         char ch = s.charAt(j);
+        //         if(set.contains(ch)) break;
         //         set.add(ch);
+
+        //         maxLen = Math.max(maxLen, j-i+1);
+
         //     }
         // }
-
-        for(int i=0; i<s.length(); i++){
-            Set<Character> set = new HashSet<>();
-            for(int j=i; j<s.length(); j++){
-                char ch = s.charAt(j);
-                if(set.contains(ch)) break;
-                set.add(ch);
-
-                maxLen = Math.max(maxLen, j-i+1);
-
-            }
-        }
 
 
         return maxLen;
