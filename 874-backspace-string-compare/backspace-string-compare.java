0b88306a03392cb4414f796ec8797a63be032bf3
Sonu Rajput <sonusinghrajput9189@gmail.com>
2026-09-21 06:33:27 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (53.10%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,24 +1,70 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
 
-        return build(s).equals(build(t));
+        int i = s.length()-1;
+        int j = t.length()-1;
+
+        int skipS = 0;
+        int skipT = 0;
+
+        while(i>=0 || j>=0){
+            while(i>=0){
+                if(s.charAt(i) == '#'){
+                    skipS++;
+                    i--;
+                }
+                else if(skipS > 0){
+                    skipS--;
+                    i--;
+                }
+                else break;
+            }
+            while(j>=0){
+                if(t.charAt(j) == '#'){
+                    skipT++;
+                    j--;
+                }
+                else if(skipT > 0){
+                    skipT--;
+                    j--;
+                }
+                else break;
+            }
+
+            if(i>=0 && j>=0){
+                if(s.charAt(i) != t.charAt(j)) return false;
+            }else{
+                if(i>=0 || j>=0) return false;
+            }
+
+            i--;
+            j--;
+        }
+        return true;
+
+
+
+
+
+
+        // return build(s).equals(build(t));
         
     }
-    private String build(String str){
-        Stack<Character> st = new Stack<>();
+    // private String build(String str){
+    //     Stack<Character> st = new Stack<>();
 
-        for(char ch : str.toCharArray()){
-            if(ch == '#'){
-                if(!st.isEmpty()) st.pop();
-            }
-            else{
-                st.push(ch);
-            }
-        }
-        StringBuilder result = new StringBuilder();
-        for(char ch : st){
-            result.append(ch);
-        }
-        return result.toString();
-    }
+    //     for(char ch : str.toCharArray()){
+    //         if(ch == '#'){
+    //             if(!st.isEmpty()) st.pop();
+    //         }
+    //         else{
+    //             st.push(ch);
+    //         }
+    //     }
+    //     StringBuilder result = new StringBuilder();
+    //     for(char ch : st){
+    //         result.append(ch);
+    //     }
+    //     return result.toString();
+    // }
 }
