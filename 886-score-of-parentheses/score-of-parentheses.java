e8f8786357136bcde26768a96aacd6d3de4a096e
Sonu Rajput <sonusinghrajput9189@gmail.com>
2026-09-24 06:48:07 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (12.18%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.java
+++ 886-score-of-parentheses/score-of-parentheses.java
@@ -1,17 +1,32 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        Stack<Integer> st = new Stack<>();
-        st.push(0);
 
-        for(char ch : s.toCharArray()){
-            if(ch == '(') st.push(0);
+        int count = 0;
+        int score = 0;
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '('){
+                count++;
+            }
             else{
-                int val = st.pop();
-                int score = Math.max(2*val,1);
-                st.push(score+st.pop());
+                count--;
+                if(s.charAt(i-1)=='(')  score += 1<<count;
             }
         }
-        return st.pop();
+        return score;
+
+        // Stack<Integer> st = new Stack<>();
+        // st.push(0);
+
+        // for(char ch : s.toCharArray()){
+        //     if(ch == '(') st.push(0);
+        //     else{
+        //         int val = st.pop();
+        //         int score = Math.max(2*val,1);
+        //         st.push(score+st.pop());
+        //     }
+        // }
+        // return st.pop();
 
     }
 }
